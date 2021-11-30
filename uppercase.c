6f46c2429ee580bcc875112b7004b57d392fab63
paj111 <92129144+paj111@users.noreply.github.com>
2021-11-30 13:05:02 +0100

    Update uppercase.c

--- uppercase.c
+++ uppercase.c
@@ -1,7 +1,14 @@
-void stringUpr(char *s){
-    int len,i;
-    len=strlen(s);
-     
-    for(i=0;i<len;i++)
-        s[i]=toupper(s[i]);
+#include "helper.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int upper(char* str)
+{
+	unsigned int i, len = strlen(str);
+
+	for(i = 0; i < len; i++){
+		str[i]=toupper(str[i]);
+	}
+	printf("\nString posle konverzije: %s\n", str);
 }
